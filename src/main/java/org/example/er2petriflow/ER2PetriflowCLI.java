b3f4fe1322fc4c2c278ef1f30d98da23a23752de
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-02-21 13:48:54 +0100

    unmarshalling first test

--- src/main/java/org/example/er2petriflow/ER2PetriflowCLI.java
+++ src/main/java/org/example/er2petriflow/ER2PetriflowCLI.java
@@ -1,9 +1,18 @@
 package org.example.er2petriflow;
 
+import org.example.er2petriflow.generated.er.GraphmlType;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+
 public class ER2PetriflowCLI {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(GraphmlType.class);
+        JAXBElement<GraphmlType> element = (JAXBElement) context.createUnmarshaller().unmarshal(ER2PetriflowCLI.class.getResourceAsStream("/test_graph.graphml"));
+        GraphmlType graph = element.getValue();
+        assert graph != null;
     }
 
 }
