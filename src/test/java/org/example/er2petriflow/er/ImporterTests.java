da658af8c365744b39b497c90e2fae8bd590dde8
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-03-03 08:24:36 +0100

    multiple entities parsing test

--- src/test/java/org/example/er2petriflow/er/ImporterTests.java
+++ src/test/java/org/example/er2petriflow/er/ImporterTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -16,6 +17,9 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ImporterTests {
+
+    private static final String ENTITY_1_NAME = "Entity1";
+    private static final String ENTITY_2_NAME = "Entity with more attributes";
 
     Importer importer;
 
@@ -38,22 +42,63 @@ public class ImporterTests {
 
         Entity entity = diagram.getEntities().get(0);
         assertNotNull(entity);
-        assertEquals("Entity", entity.getName());
+        assertEquals("Entity1", entity.getName());
+        testEntity1(entity);
+    }
 
-        List<Attribute> attributes = entity.getAttributes();
-        assertNotNull(attributes);
-        assertEquals(2, attributes.size());
+    @Test
+    @DisplayName("Should import multiple entities")
+    void importMultipleEntities() {
+        Optional<ERDiagram> result = importer.importDiagram(getTestFile("MultipleEntities"));
+        assertTrue(result.isPresent());
 
-        Set<String> titles = new java.util.HashSet<>(Set.of("Attribute1", "Attribute2"));
+        ERDiagram diagram = result.get();
+        assertNotNull(diagram.getRelations());
+        assertEquals(0, diagram.getRelations().size());
+        assertNotNull(diagram.getEntities());
+        assertEquals(2, diagram.getEntities().size());
 
-        for (Attribute a : attributes) {
-            assertEquals(AttributeType.TEXT, a.getType());
-            assertTrue(titles.contains(a.getName()));
-            titles.remove(a.getName());
+        Set<String> titles = new HashSet<>(Set.of(ENTITY_1_NAME, ENTITY_2_NAME));
+
+        for (Entity e : diagram.getEntities()) {
+            assertNotNull(e);
+            assertTrue(titles.contains(e.getName()));
+            titles.remove(e.getName());
+            switch (e.getName()) {
+                case ENTITY_1_NAME:
+                    testEntity1(e);
+                    break;
+                case ENTITY_2_NAME:
+                    testEntity2(e);
+                    break;
+            }
         }
     }
 
     private InputStream getTestFile(String fileName) {
         return ImporterTests.class.getResourceAsStream("/" + fileName + ".erdplus");
+    }
+
+    private void testEntity1(Entity entity) {
+        testAttributes(entity, Set.of("Attribute1", "Attribute2"), AttributeType.TEXT);
+    }
+
+    private void testEntity2(Entity entity) {
+        testAttributes(entity, Set.of("New Attribute", "NewAttribute2", "NewAttribute3", "NewAttribute4"), AttributeType.NUMBER);
+    }
+
+    private void testAttributes(Entity entity, Set<String> expectedTitles, AttributeType expectedType) {
+        List<Attribute> attributes = entity.getAttributes();
+        int expectedCount = expectedTitles.size();
+
+        assertNotNull(attributes);
+        assertEquals(expectedCount, attributes.size());
+
+        Set<String> titles = new HashSet<>(expectedTitles);
+        for (Attribute a : attributes) {
+            assertEquals(expectedType, a.getType());
+            assertTrue(titles.contains(a.getName()));
+            titles.remove(a.getName());
+        }
     }
 }
